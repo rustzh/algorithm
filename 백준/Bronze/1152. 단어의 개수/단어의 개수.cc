22788ea977ec133907da4d9493c5140b72a1941b
최휘녕 <94417319+rustzh@users.noreply.github.com>
2025-10-29 21:19:30 +0900

    [Bronze II] Title: 단어의 개수, Time: 20 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mx 1000000
+
+int main()
+{
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);   
+
+   int cnt = 0;
+   string s;
+   
+   while(cin >> s) {
+   	cnt ++;
+   }
+
+   cout << cnt;
+
+   return 0;
+}
