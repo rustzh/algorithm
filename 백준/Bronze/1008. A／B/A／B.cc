0fa4009dbcb486fd4cba21167b2788389b0c734a
최휘녕 <94417319+rustzh@users.noreply.github.com>
2025-10-29 21:18:17 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);   
+
+   double a, b;
+
+   cout.precision(15);
+   cin >> a >> b;
+   cout << a/b;
+
+   return 0;
+}
