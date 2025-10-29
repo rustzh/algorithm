1b023835251c4718fee6fa602ddd74b92b5bafbc
최휘녕 <94417319+rustzh@users.noreply.github.com>
2025-10-29 21:20:54 +0900

    [Silver V] Title: 영화감독 숌, Time: 588 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+cnt = 0
+result = 666
+
+while True:
+	if '666' in str(result):
+		cnt += 1
+	if cnt == N:
+		break
+	result += 1
+
+print(result)
